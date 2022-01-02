d08a4fa3df03092c452c699804630d1355ce9730
minji <mini2ft@gmail.com>
2022-01-02 21:41:58 +0900

    create project

---
+++ src/Test/Test001.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Test001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hellow minji world");
+		System.out.println("실행하기는 Ctrl + F11");
+
+	}
+
+}
