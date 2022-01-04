488aae8a35c497a3a43e3d89715b52493b775873
minji <mini2ft@gmail.com>
2022-01-04 22:53:19 +0900

    코드업 1001~1008

---
+++ src/codeup/CodeUp_001.java
@@ -0,0 +1,38 @@
+package codeup;
+
+public class CodeUp_001 {
+	
+	// 답안 참고
+	// https://girincode.tistory.com/32
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello"); // 1001
+		
+		System.out.println("Hello World"); // 1002
+		
+		System.out.print("Hello\nWorld"); // 1003
+		
+		System.out.println("'Hello'"); // 1004
+		
+		System.out.println("\"Hello World\""); // 1005
+		
+		System.out.println("\"!@#$%^&*()\""); // 1006
+		
+		System.out.println("\"C:\\Download\\hello.cpp\""); // 1007
+		
+		
+		// 1008
+	    System.out.println("\u250C\u252C\u2510");
+	    System.out.println("\u251C\u253C\u2524");
+	    System.out.println("\u2514\u2534\u2518");
+		
+	}
+}
+
+//public class Main {
+//	public static void main(String[] args) {
+//		
+//	}
+//	
+//}
