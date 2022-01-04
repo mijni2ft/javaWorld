d3c826a8bf032d78e94f2b0a820036d7f1021cac
minji <mini2ft@gmail.com>
2022-01-04 23:00:25 +0900

    코드업 1010~1015

---
+++ src/codeup/CodeUp_002.java
@@ -0,0 +1,62 @@
+package codeup;
+
+import java.util.Scanner;
+public class CodeUp_002 {
+	
+	// 1015
+	public static void main(String[] args) {
+		float a;
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextFloat();
+		System.out.printf("%.2f",a);
+	}
+	
+	// 1015
+//	public static void main(String[] args) {
+//		float a;
+//		Scanner scan = new Scanner(System.in);
+//		a = scan.nextFloat();
+//		System.out.printf("%.2f",a);
+//	}
+	
+//	// 1014
+//	public static void main(String[] args) {
+//		String a, b;
+//		Scanner scan = new Scanner(System.in);
+//		a = scan.next();
+//		b = scan.next();
+//		System.out.print(b + " " + a);
+//	}
+	
+//	// 1013
+//	public static void main(String[] args) {
+//		int a, b;
+//		Scanner scan = new Scanner(System.in);
+//		a = scan.nextInt();
+//		b = scan.nextInt();
+//		System.out.print(a + " " + b);
+//	}
+	
+//	// 1012
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		float num = scan.nextFloat();
+//		System.out.printf("%f",num); // 소수점 형식으로 출력
+//	}
+	
+	// 1011
+//	public static void main(String[] args) {
+//		String str;
+//		Scanner scan = new Scanner(System.in);
+//		str = scan.next();
+//		System.out.print(str);
+//	}
+	
+	// 1010
+//	public static void main(String[] args) {
+//		int a;
+//		Scanner scan = new Scanner(System.in);
+//		a = scan.nextInt();
+//		System.out.print(a);
+//	}
+}
